9e3404951b31fa99dc41deba1597de6b6dd72f19
Nikhil Eligar <91929230+Nikhileligar@users.noreply.github.com>
2024-01-03 19:39:23 +0530

    Create adminSchema.js

---
+++ task_01/backend/model/adminSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin'],
+    default: 'admin',
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// const Admin = mongoose.model('Admin', AdminSchema);
+const Admin = mongoose.models.Admin || mongoose.model ("Admin", AdminSchema)
+export default Admin;
